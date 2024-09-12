bad9591066bfb56f517ac9f9881c8ce15f0c8b9c
Joe Nelson <jnelson136@my.wctc.edu>
2024-09-12 12:34:17 -0500

    Re Publish

--- obj/Release/net8.0/fiftyfiftybalance.AssemblyInfo.cs
+++ obj/Release/net8.0/fiftyfiftybalance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fiftyfiftybalance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89b8585a9881c516fd18c6582a192fc28d4d8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc31214b63392ddd2790becab3e7fac6576b7fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("fiftyfiftybalance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fiftyfiftybalance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
